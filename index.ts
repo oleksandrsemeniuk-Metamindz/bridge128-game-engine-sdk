41162d4f13f80d0770a43215e35341fc79a8d9ab
Oleksandr Semeniuk <oleksandrsem@metamindz.co.uk>
2024-06-21 17:54:01 +0300

    feat: Export needed entities.

---
+++ index.ts
@@ -0,0 +1,8 @@
+import 'module-alias/register.js';
+
+export { RoomMode } from '~src/constants/room-mode.enum';
+export { Bridge128GameEngine } from '~src/core/bridge128-game-engine';
+export { CreateRoomPayload } from '~src/types/create-room-payload';
+export { CreatedRoomInfo } from '~src/types/created-room-info';
+export { GameEngineClientOptions } from '~src/types/game-engine-client-options';
+export { GameEngineRoomCreationException } from '~src/utils/exceptions';
